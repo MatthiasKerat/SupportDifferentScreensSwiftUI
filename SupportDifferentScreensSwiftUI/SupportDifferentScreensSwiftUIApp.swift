0cc89007a34f9dd0e08c5ff887793c91f018e541
Matthias Kerat <MatthiasKerat@web.de>
2022-09-25 20:24:21 +0200

    Initial Commit

---
+++ SupportDifferentScreensSwiftUI/SupportDifferentScreensSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupportDifferentScreensSwiftUIApp.swift
+//  SupportDifferentScreensSwiftUI
+//
+//  Created by Matthias Kerat on 25.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SupportDifferentScreensSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
